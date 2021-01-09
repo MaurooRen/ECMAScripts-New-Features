02a7c70ae9732b2ec1f70732c7f38680cb09c6ce
MaurooRen <maurooren@gmail.com>
2021-01-09 12:11:27 -0500

    .trim

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,2 +1,17 @@
-var array = [1,2,3, [1,2,3, [1,2,3]]];
+let array = [1,2,3, [1,2,3, [1,2,3]]];
 console.log(array.flat())
+
+
+let array2 = [1,2,3,4,5]
+console.log(array2.flatMap(value => [value, value * 2]))
+
+
+//Delete spaces at the beginning
+let hello = '                  hello world'
+console.log(hello)
+console.log(hello.trimStart())
+
+//Delete spaces at the end
+let hello2 = 'hello world        '
+console.log(hello2)
+console.log(hello2.trimEnd())

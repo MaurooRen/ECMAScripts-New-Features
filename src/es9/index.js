aad77a659c141f4739aaec24b9466ebea1663674
MaurooRen <maurooren@gmail.com>
2021-01-09 11:57:30 -0500

    using regex

--- src/es9/index.js
+++ src/es9/index.js
@@ -26,7 +26,8 @@ console.log(obj2)
 const helloWorld = () => {
     return new Promise((resolve, reject) => {
         (true)
-            ? resolve('Yey!')
+            // ? resolve('Yey!')
+            ? setTimeout(() => resolve('Yey! after some time'), 3000)
             : reject(new Error('Test Error'))
     })
 }
@@ -35,3 +36,14 @@ helloWorld()
     .then(response => console.log(response))
     .catch(error => console.log(error))
     .finally(() => console.log('This is the end')) //This property allow us to execute code one the entire execution has end
+
+
+
+
+    const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+    const match = regexData.exec('2018-04-20')
+    const year = match[1]
+    const month = match[2]
+    const day = match[3]
+
+    console.log(year, month, day)

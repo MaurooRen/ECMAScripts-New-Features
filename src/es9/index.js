98fbfc31e25928feea3c6472cd471b19b0508bc9
MaurooRen <maurooren@gmail.com>
2021-01-09 11:35:13 -0500

    ussing FINALLY with Promise

--- src/es9/index.js
+++ src/es9/index.js
@@ -20,3 +20,18 @@ const obj2 = {
 }
 
 console.log(obj2)
+
+
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? resolve('Yey!')
+            : reject(new Error('Test Error'))
+    })
+}
+
+helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('This is the end')) //This property allow us to execute code one the entire execution has end
